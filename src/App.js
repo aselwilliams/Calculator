5d4c030f3c61a0401b22e215c5f844a072297220
aselwilliams <aselwilliams22@gmail.com>
2022-06-23 09:04:47 -0400

    painted the ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React from 'react'
 import './App.css';
 
 function App() {
+  const numbers=[1,2,3,4,5,6,7,8,9]
+  const operations=["+","-","*","/"]
   return (
     <div className="App">
-     
+      <main className='calculator'>
+      <div className='display'>dddd
+      </div>
+      <div className='buttons'>
+        <section className='left-side'>
+          <div id='clear-btn'>Clear</div>
+      <div className='numbers'>
+        {numbers.map((val,key)=>{
+          return <div id='indiv-number'>{val}</div>
+        })}
+      </div>
+      </section>
+
+      <section className='right-side'>
+        {operations.map((val,key)=>{
+          return <div id='operation'>{val}</div>
+        })}
+      </section>
+      </div>
+      </main>
     </div>
+    
   );
 }
 
